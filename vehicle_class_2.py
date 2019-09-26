b4084eb43b6961b0849b056b7b1d544940fdc966
Rory Stokes <RStokes@spartaglobal.com>
2019-09-26 15:42:57 +0100

    new vehicle2 class

---
+++ vehicle_class_2.py
@@ -0,0 +1,12 @@
+class Vehicle2():
+    def __init__(self, wheels, capacity, colour):
+        self.wheels = wheels
+        self.capacity = capacity
+        self.colour = colour
+
+    def accelerate(self):
+        return 'vrooom'
+
+    def male_sound(self):
+        return '*noises*'
+    
